980ee0b71033cde67279ec27ef143c9edf560d3d
mosesintech <mosesmaxcosme@gmail.com>
2024-10-25 12:55:15 -0500

    feat(scenes): add missing pieces for IronclawPort to load

--- Resources/TSX/CL_Crafting.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="CL_Crafting" tilewidth="32" tileheight="32" tilecount="435" columns="29">
- <image source="../Sheets/Locations/CL_Crafting.png" width="928" height="480"/>
-</tileset>
